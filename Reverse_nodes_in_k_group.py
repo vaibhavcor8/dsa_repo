0d8005fdec6012022d2a25f35b0ac71d5dfef1b2
Vaibhav Srivastava <vaibhavcorporate8@gmail.com>
2026-02-08 10:53:30 +0000

    first commit

---
+++ Reverse_nodes_in_k_group.py
@@ -0,0 +1,23 @@
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+
+        curr = head
+        for _ in range(k):
+            if not curr:
+                return head
+            curr = curr.next
+        
+    
+        prev = None
+        curr = head
+        for _ in range(k):
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        
+
+        if head:
+            head.next = self.reverseKGroup(curr, k)
+            
+        return prev
