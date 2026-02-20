9a47b84de2cebebebdfbdf2f290f8f877d9e1506
Vaibhav Srivastava <vaibhavcorporate8@gmail.com>
2026-02-20 02:36:44 +0000

    second commit

--- Sudoku_solver.py
+++ Sudoku_solver.py
@@ -1,41 +1,47 @@
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        self.solve(board)
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        empty_cells = []
 
-    def solve(self, board):
+        # Step 1: Pre-populate our sets and find all empty spots
         for r in range(9):
             for c in range(9):
                 if board[r][c] == ".":
-                    for num in "123456789":
-                        if self.is_safe(board, r, c, num):
-                            board[r][c] = num
-                            
-                            # If this guess leads to a solution, stop
-                            if self.solve(board):
-                                return True
-                            
-                            # Otherwise, backtrack
-                            board[r][c] = "."
-                            
-                    return False  # Trigger backtracking if no numbers 1-9 work
-        return True # Board is full
+                    empty_cells.append((r, c))
+                else:
+                    val = board[r][c]
+                    rows[r].add(val)
+                    cols[c].add(val)
+                    boxes[(r // 3) * 3 + (c // 3)].add(val)
 
-    def is_safe(self, board, r, c, num):
-      
-        for i in range(9):
-            if board[r][i] == num or board[i][c] == num:
-                return False
-        
-        # Check 3x3 sub-box
-        start_row, start_col = 3 * (r // 3), 3 * (c // 3)
-        for i in range(start_row, start_row + 3):
-            for j in range(start_col, start_col + 3):
-                if board[i][j] == num:
-                    return False
-                    
-        return True
+        def backtrack(index):
+            if index == len(empty_cells):
+                return True  # All cells filled!
+
+            r, c = empty_cells[index]
+            box_idx = (r // 3) * 3 + (c // 3)
+
+            for num in "123456789":
+                if num not in rows[r] and num not in cols[c] and num not in boxes[box_idx]:
+                    # Place the number
+                    board[r][c] = num
+                    rows[r].add(num)
+                    cols[c].add(num)
+                    boxes[box_idx].add(num)
+
+                    if backtrack(index + 1):
+                        return True
+
+                    # Undo (Backtrack)
+                    rows[r].remove(num)
+                    cols[c].remove(num)
+                    boxes[box_idx].remove(num)
+                    board[r][c] = "."
+            
+            return False
+
+        backtrack(0)
 
         # first commit
