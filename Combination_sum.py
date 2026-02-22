ac0903e0b2ff58abd0db5e178da07d61b791d210
Vaibhav Srivastava <vaibhavcorporate8@gmail.com>
2026-02-22 10:32:29 +0000

    first commit

---
+++ Combination_sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        
+        def backtrack(remaining, combo, start_index):
+         
+            if remaining == 0:
+                result.append(list(combo))
+                return
+        
+            if remaining < 0:
+                return
+            
+            for i in range(start_index, len(candidates)):
+          
+                combo.append(candidates[i])
+                
+                backtrack(remaining - candidates[i], combo, i)
+                
+                combo.pop()
+                
+        backtrack(target, [], 0)
+        return result
