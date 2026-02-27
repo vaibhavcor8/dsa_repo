8e2cb700f0ff9601c0a064d48c45c71ce21d8ddc
Vaibhav Srivastava <vaibhavcorporate8@gmail.com>
2026-02-27 02:13:04 +0000

    first commit

---
+++ Wildcard_matching.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        s_ptr = 0
+        p_ptr = 0
+        star_idx = -1
+        s_tmp_idx = -1
+        
+        while s_ptr < len(s):
+       
+            if p_ptr < len(p) and (p[p_ptr] == s[s_ptr] or p[p_ptr] == '?'):
+                s_ptr += 1
+                p_ptr += 1
+            
+        
+            elif p_ptr < len(p) and p[p_ptr] == '*':
+       
+                star_idx = p_ptr
+                s_tmp_idx = s_ptr
+             
+                p_ptr += 1
+                
+     
+            elif star_idx != -1:
+              
+                p_ptr = star_idx + 1
+          
+                s_tmp_idx += 1
+                s_ptr = s_tmp_idx
+                
+          
+            else:
+                return False
+        
+        return all(x == '*' for x in p[p_ptr:])
