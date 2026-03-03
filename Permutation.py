ba5c35a6896e965ded5f6f8c158869bd91b19817
Vaibhav Srivastava <vaibhavcorporate8@gmail.com>
2026-03-03 15:50:07 +0000

    first commit

---
+++ Permutation.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        
+        def backtrack(start):
+          
+            if start == n:
+                res.append(list(nums)) 
+                return
+            
+            for i in range(start, n):
+            
+                nums[start], nums[i] = nums[i], nums[start]
+         
+                backtrack(start + 1)
+                
+                nums[start], nums[i] = nums[i], nums[start]
+        
+        backtrack(0)
+        return res
