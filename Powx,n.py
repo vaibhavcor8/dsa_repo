be5544817dcb2bb5e8c966186b3dbe0382b695b6
Vaibhav Srivastava <vaibhavcorporate8@gmail.com>
2026-03-09 04:56:51 +0000

    first commit

---
+++ Powx,n.py
@@ -0,0 +1,19 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+
+        if n < 0:
+            x = 1 / x
+            n = -n
+        
+        res = 1
+        current_product = x
+        
+        while n > 0:
+         
+            if n % 2 == 1:
+                res *= current_product
+            
+            current_product *= current_product
+            n //= 2
+            
+        return res
