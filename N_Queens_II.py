59f86d437568c4d986db74f0b92aa75eeec19b76
Vaibhav Srivastava <vaibhavcorporate8@gmail.com>
2026-03-11 02:05:19 +0000

    first commit

---
+++ N_Queens_II.py
@@ -0,0 +1,31 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        self.count = 0
+        cols = set()
+        pos_diag = set() 
+        neg_diag = set() 
+        
+        def backtrack(r):
+         
+            if r == n:
+                self.count += 1
+                return
+            
+            for c in range(n):
+                
+                if c in cols or (r + c) in pos_diag or (r - c) in neg_diag:
+                    continue
+                
+                # Place queen (add to constraints)
+                cols.add(c)
+                pos_diag.add(r + c)
+                neg_diag.add(r - c)
+                
+                backtrack(r + 1)
+                
+                cols.remove(c)
+                pos_diag.remove(r + c)
+                neg_diag.remove(r - c)
+        
+        backtrack(0)
+        return self.count
