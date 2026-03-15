c86626b7e3c830491730dc4606c94f456d7036b2
Vaibhav Srivastava <vaibhavcorporate8@gmail.com>
2026-03-15 08:47:33 +0000

    first commit

---
+++ Jump_game.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_reach = 0
+        target = len(nums) - 1
+        
+        for i, jump in enumerate(nums):
+
+            if i > max_reach:
+                return False
+        
+            max_reach = max(max_reach, i + jump)
+            
+            if max_reach >= target:
+                return True
+                
+        return True
