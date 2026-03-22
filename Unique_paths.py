66936ece70ea2511561c253cc714defe84b85e95
Vaibhav Srivastava <vaibhavcorporate8@gmail.com>
2026-03-22 12:49:53 +0000

    first commit

---
+++ Unique_paths.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        row = [1] * n        
+        
+        for i in range(1, m):
+     
+            for j in range(1, n):
+
+                row[j] = row[j] + row[j - 1]
+                
+        return row[-1]
