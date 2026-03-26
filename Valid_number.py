8657561a887246aa8718c4dcdc9d38cc5a41b739
Vaibhav Srivastava <vaibhavcorporate8@gmail.com>
2026-03-26 10:29:10 +0000

    first commit

---
+++ Valid_number.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        seen_digit = False
+        seen_dot = False
+        seen_exponent = False
+        
+        for i, char in enumerate(s):
+            if char.isdigit():
+                seen_digit = True
+            
+            elif char in "+-":
+                
+                if i > 0 and s[i-1] not in "eE":
+                    return False
+            
+            elif char in "eE":
+       
+                if seen_exponent or not seen_digit:
+                    return False
+                seen_exponent = True
+         
+                seen_digit = False
+                
+            elif char == ".":
+        
+                if seen_dot or seen_exponent:
+                    return False
+                seen_dot = True
+            
+            else:
+             
+                return False
+        
+        return seen_digit
