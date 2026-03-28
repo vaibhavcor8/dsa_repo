3683f75fef9c83f89ebc57282614411a0769f160
Vaibhav Srivastava <vaibhavcorporate8@gmail.com>
2026-03-28 11:27:48 +0000

    first commit

---
+++ Add_binary.py
@@ -0,0 +1,22 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = []
+        carry = 0
+        i, j = len(a) - 1, len(b) - 1
+        
+        while i >= 0 or j >= 0 or carry:
+       
+            digit_a = int(a[i]) if i >= 0 else 0
+      
+            digit_b = int(b[j]) if j >= 0 else 0
+            
+            total = digit_a + digit_b + carry
+            
+            res.append(str(total % 2))
+     
+            carry = total // 2
+            
+            i -= 1
+            j -= 1
+            
+        return "".join(res[::-1])
