ab0063509e392651c297c22ddf6c024dd8e76c4a
Vaibhav Srivastava <vaibhavcorporate8@gmail.com>
2026-03-29 09:39:25 +0000

    first commit

---
+++ Text_justification.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        res, cur_line, cur_len = [], [], 0
+        
+        for w in words:
+            
+            if cur_len + len(w) + len(cur_line) > maxWidth:
+               
+                for i in range(maxWidth - cur_len):
+                    cur_line[i % (len(cur_line) - 1 or 1)] += ' '
+                res.append("".join(cur_line))
+                cur_line, cur_len = [], 0
+            
+            cur_line.append(w)
+            cur_len += len(w)
+            
+        last_line = " ".join(cur_line)
+        res.append(last_line + ' ' * (maxWidth - len(last_line)))
+        
+        return res
